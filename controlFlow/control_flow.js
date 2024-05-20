f345aae16c1205e35809ff9e5079c5ef3a42aeb5
nikhilesh36 <nikhileshk4545@gmail.com>
2024-05-20 11:53:24 -0400

    message

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,12 @@
+let isLoggedIn = true;
+let userMessage;
+if(isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+console.log("User Message:", userMessage);
